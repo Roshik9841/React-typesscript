9ab112247a84d562a5fb51bd01f9238d2f381c92
I am Roshik <Roshik9841@gmail.com>
2025-07-22 12:12:30 +0545

    Generic value with typeScript

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import "./App.css";
 // import Hook from './Hook'
 // import Used from "./Used";
 import { Counter } from "./components/class/Counter";
+import { List } from "./components/class/List";
 function App() {
   return (
     <div className="App">
     {/* <Used/> */}
       {/* <Hook/> */}
       <Counter message={"The count value is"}/>
+      <List items={['roshik','ragini','dasdas']}
+      onClick= {(item)=>console.log(item)}/>
+
+      <List items={[1,2,41]}
+      onClick= {(item)=>console.log(item)}/>
     </div>
   );
 }
